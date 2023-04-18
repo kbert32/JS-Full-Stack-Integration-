58f71733a3cdb0da7ab8a7f32c5f9394a02781b7
kbert32 <103608624+kbert32@users.noreply.github.com>
2023-04-17 20:20:36 -0600

    5 done

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -1,12 +1,15 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 
 import Card from '../../shared/components/UIElements/Card';
 import Button from '../../shared/components/FormElements/Button';
 import Modal from '../../shared/components/UIElements/Modal';
 import Map from '../../shared/components/UIElements/Map';
+import { AuthContext } from '../../shared/context/auth-context';
 import './PlaceItem.css';
 
 export default function PlaceItem(props) {
+
+    const authCtx = useContext(AuthContext);
 
     const [showConfirm, setShowConfirm] = useState(false);
     const [showMap, setShowMap] = useState(false);
@@ -66,8 +69,8 @@ export default function PlaceItem(props) {
                     </div>
                     <div className='place-item__actions'>
                         <Button inverse onClick={openMapHandler}>VIEW ON MAP</Button>
-                        <Button to={`/places/${props.id}`}>EDIT</Button>
-                        <Button onClick={showDeleteWarningHandler} danger>DELETE</Button>
+                        {authCtx.isLoggedIn && <Button to={`/places/${props.id}`}>EDIT</Button>}
+                        {authCtx.isLoggedIn && <Button onClick={showDeleteWarningHandler} danger>DELETE</Button>}
                     </div>
                 </Card>
             </li>
