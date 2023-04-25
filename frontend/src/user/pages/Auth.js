68f5ea0fc4177ab096d23db9ad941e926a2d926f
kbert32 <103608624+kbert32@users.noreply.github.com>
2023-04-25 14:52:30 -0600

    10

--- frontend/src/user/pages/Auth.js
+++ frontend/src/user/pages/Auth.js
@@ -43,7 +43,7 @@ export default function Auth() {
             }, false);
         }
         setIsLoginMode(prevMode => !prevMode);
-        console.log(isLoginMode);
+        
     };
 
     async function loginHandler(event) {
