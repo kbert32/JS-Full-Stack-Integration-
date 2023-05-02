40c837f668c385f225d804c4d571e3673c0e0b7d
kbert32 <103608624+kbert32@users.noreply.github.com>
2023-05-01 21:19:08 -0600

    13

--- backend/App.js
+++ backend/App.js
@@ -57,7 +57,7 @@ app.use((error, req, res, next) => {        //this function will only be execute
 mongoose
     .connect(url)
     .then(() => {
-        app.listen(5000);
+        app.listen(process.env.PORT || 5000);
     })
     .catch(err => {
         console.log(err);
